d85aff4d1bcbdaadaae1b5e9136246546bb0f9f7
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2023-01-10 12:36:33 +0300

    edit2

--- Seminar6Task1/src/Seminar6Task1.java
+++ Seminar6Task1/src/Seminar6Task1.java
@@ -1,108 +1,68 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-
 public class Seminar6Task1 {
     public static void main(String[] args) {
-        Find (5, 5);
+        System.out.println("Start point: x = 5, y = 7");
+        System.out.println(rawData(create()));
+        System.out.println("Result:");
+        System.out.println(rawData(find(5, 7)));
     }
-    public static int[][] map ={
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-            {-1, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, -1, 00, 00, -1, 00, 00, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, -1, 00, -1, -1, -1, -1, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1},
-            {-1, -1, -1, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, -1, -1, 00, 00, -1},
-            {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-            {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, -1, 00, 00, 00, -1},
-            {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}};
 
-    public static void Find(int x, int y) {
+    public static int[][] create() {
+        int[][] map3 = {
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, -1, 00, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, -1, 00, -1, -1, -1, -1, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1},
+                {-1, -1, -1, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, -1, -1, 00, 00, -1},
+                {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, -1, 00, 00, 00, -1},
+                {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}};
+        return map3;
+    }
+
+    public static int[][] find(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        int[] point = {x, y};
-        queue.add(point);
-        map[x][y] = 1;
-
+        queue.add(new int[]{x, y});
+        int[][] map4 = create();
+        map4[x][y] = 1;
         while (queue.size() != 0) {
-            int[] p = queue.remove();
-
-            if (map[x - 1][y] == 0) {
-                point[0] = x - 1;
-                point[1] = y;
-                queue.add(point);
-                map[x - 1][y] = map[x][y] + 1;
+            int[] temp = queue.remove();
+            x = temp[0];
+            y = temp[1];
+            if (map4[x - 1][y] == 0) {
+                queue.add(new int[]{x - 1, y});
+                map4[x - 1][y] = map4[x][y] + 1;
             }
-            if (map[x][y - 1] == 0) {
-                point[0] = x;
-                point[1] = y - 1;
-                queue.add(point);
-                map[x][y - 1] = map[x][y] + 1;
+            if (map4[x][y - 1] == 0) {
+                queue.add(new int[]{x, y - 1});
+                map4[x][y - 1] = map4[x][y] + 1;
             }
-            if (map[x + 1][y] == 0) {
-                point[0] = x + 1;
-                point[1] = y;
-                queue.add(point);
-                map[x + 1][y] = map[x][y] + 1;
+            if (map4[x + 1][y] == 0) {
+                queue.add(new int[]{x + 1, y});
+                map4[x + 1][y] = map4[x][y] + 1;
             }
-            if (map[x][y + 1] == 0) {
-                point[0] = x;
-                point[1] = y + 1;
-                queue.add(point);
-                map[x][y + 1] = map[x][y] + 1;
+            if (map4[x][y + 1] == 0) {
+                queue.add(new int[]{x, y + 1});
+                map4[x][y + 1] = map4[x][y] + 1;
             }
         }
-    }
-}
-
-class MapPrinter {
-
-    public MapPrinter() {
+        return map4;
     }
 
-    public String rawData(int[][] map) {
+    public static String rawData(int[][] map1) {
         StringBuilder sb = new StringBuilder();
-
-        for (int row = 0; row < map.length; row++) {
-            for (int col = 0; col < map[row].length; col++) {
-                sb.append(String.format("%5d", map[row][col]));
-            }
-            sb.append("\n");
-        }
-        for (int i = 0; i < 3; i++) {
-            sb.append("\n");
-        }
-
-        return sb.toString();
-    }
-
-    public String mapColor(int[][] map) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int row = 0; row < map.length; row++) {
-            for (int col = 0; col < map[row].length; col++) {
-                switch (map[row][col]) {
-                    case 0:
-                        sb.append("в–‘");
-                        break;
-                    case -1:
-                        sb.append("в–“");
-                        break;
-                    case -2:
-                        sb.append("K");
-                        break;
-                    case -3:
-                        sb.append("E");
-                        break;
-                    default:
-                        break;
-                }
+        for (int row = 0; row < map1.length; row++) {
+            for (int col = 0; col < map1[row].length; col++) {
+                sb.append(String.format("%5d", map1[row][col]));
             }
             sb.append("\n");
         }
@@ -111,7 +71,4 @@ class MapPrinter {
         }
         return sb.toString();
     }
-
 }
-
-
