d2b350977aa04dff945755ceae675bbdff0e1625
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2023-01-28 21:00:42 +0300

    Seminar2

---
+++ Seminar2/src/Grandpa.java
@@ -0,0 +1,16 @@
+public class Grandpa implements  Mann{
+
+    int age;
+    String name;
+
+
+    @Override
+    public void getAge() {
+        this.age = age;
+    }
+
+    @Override
+    public void getName() {
+        this.name = name;
+    }
+}
---
+++ Seminar2/src/Mann.java
@@ -0,0 +1,8 @@
+public interface Mann {
+    void getAge();
+    default void getAge2(){
+
+    }
+    void getName();
+    void showInfo();
+}
