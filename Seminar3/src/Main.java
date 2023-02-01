9237584c9e3cc242ebcf3fa77869771f32973374
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2023-02-01 12:48:32 +0300

    edit

--- Seminar3/src/Main.java
+++ Seminar3/src/Main.java
@@ -1,51 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Main {
+public class Main {
     public static void main(String[] args) {
-        List<List<String>> fathers = new ArrayList<>();
-        List<List<String>> sons = new ArrayList<>();
+        /**
+         * Создание экзепляра интерфейса дискриминанта с лямбда-функцией
+         */
+        I__Discriminant disk = (a, b, c) -> (b * b - 4 * a * c) > 0;
 
-        Grandpa grandpa = new Grandpa("Anatoliy", 67);
+        int a = 1, b = -15, c = 36;
 
-        Father father1 = new Father("Dmitriy", 36, "Anatoliy");
-        fathers.add(father1.addFatherList());
-
-        Father father2 = new Father("Mikhail", 36, "Anatoliy");
-        fathers.add(father2.addFatherList());
-
-        Son son1 = new Son("Leonid", 6, "Anatoliy", "Dmitriy");
-        sons.add(son1.addGrandpaList());
-        fathers.add(son1.addFatherList());
-
-        Son son2 = new Son("Semion", 8, "Anatoliy", "Dmitriy");
-        sons.add(son2.addGrandpaList());
-        fathers.add(son2.addFatherList());
-
-        Son son3 = new Son("Aleksey", 8, "Anatoliy", "Mikhail");
-        sons.add(son3.addGrandpaList());
-        fathers.add(son3.addFatherList());
-
-        grandpa.showInfo();
-        father1.showInfo();
-        father2.showInfo();
-        son1.showInfo();
-        son2.showInfo();
-        son3.showInfo();
-
-        System.out.println("Grandsons of " + grandpa.name);
-        grandpa.showGrandson(sons);
-        System.out.println();
-
-        System.out.println("Sons of " + grandpa.name);
-        grandpa.showSon(fathers);
-        System.out.println();
-
-        System.out.println("Sons of " + father1.name);
-        father1.showSon(fathers);
-        System.out.println();
-
-        System.out.println("Sons of " + father2.name);
-        father2.showSon(fathers);
+        /**
+         * Нахождение корней уравнения
+         */
+        if (disk.square(a, b, c)) {
+            System.out.print("x1 = " + (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a) + ", x2 = " + (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a));
+        } else {
+            System.out.print("Действительных корней нет");
+        }
     }
 }
--- Seminar3/src/Mann.java
+++
@@ -1,3 +0,0 @@
-public interface Mann {
-    void showInfo();
-}
